e0965a12747e9482a95bf71949ff0c87e825f4e5
unknown <gstefani@SW-GBHA-SHEL311.emea.wdpr.disney.com>
2014-12-19 12:13:06 +0000

    adding some ajax calls to an open API and using some JQueryUI stuff

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	'use strict';
+	setTimeout(function(){
+		console.log('Hey');
+	}, 7000);
+
+	$.ajax({
+		url: 'https://cohesiondata.ec.europa.eu/resource/ujd7-ywyx.json?$offset=0'
+	}).done(function(data){
+		$('#depressingTable').append('<tbody>');
+		$.each(data, function(index, value){
+			$('#depressingTable').append('<tr><td>'+index+'</td><td>'+ value.nuts_code +'</td><td>'+ value.nuts_name +'</td><td>'+ value.unemployment_rate_2012 +'</td></tr>');
+		});
+		$('#depressingTable').append('</tbody>');
+		$('#depressingTable tbody').sortable().disableSelection();
+	}).fail(function(){
+		$('#depressingTable tr').append('<tr><td>Something went wrong!</td><td> :( </td><td> :( </td><td> :( </td></tr>');
+	});
+});
